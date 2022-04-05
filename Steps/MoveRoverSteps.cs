0c4da55faa034484b37607e87ae79b19e7ab048d
Sameer Nazeer <sameernln@gmail.com>
2022-04-05 15:21:41 +0530

    Naming convention fix for roverContext

--- Steps/MoveRoverSteps.cs
+++ Steps/MoveRoverSteps.cs
@@ -11,41 +11,41 @@ namespace MarsRovers.Steps
     [Binding]
     class MoveRoverSteps
     {
-        private readonly RoverTestContext _roverTestContext;
-        public MoveRoverSteps(RoverTestContext roverTestContext)
+        private readonly RoverTestContext _roverContext;
+        public MoveRoverSteps(RoverTestContext roverContext)
         {
-            _roverTestContext = roverTestContext;
+            _roverContext = roverContext;
         }
         [Given(@"Rover is at (\d+), (\d+)")]
         public void GivenRoverIsAt(int x, int y)
         {
             Point position = new Point(x, y);
-            _roverTestContext._rover = new Rover();
-            _roverTestContext._rover.SetPosition(position);
+            _roverContext._rover = new Rover();
+            _roverContext._rover.SetPosition(position);
         }
 
         [Given(@"rover is pointing towards ([NEWS]{1})")]
         public void GivenRoverIsPointingTowardsDirection(string direction)
         {
-            _roverTestContext._rover.SetDirection(direction);
+            _roverContext._rover.SetDirection(direction);
         }
 
         [When(@"rover moves forward")]
         public void WhenRoverMovesForward()
         {
-            _roverTestContext._rover.Move();
+            _roverContext._rover.Move();
         }
 
         [Then(@"rover should be at (\d+), (\d+)")]
         public void ThenRoverShouldBeAt(int x, int y)
         {
-            _roverTestContext._rover.GetPosition().Should().Be(new Point(x, y));
+            _roverContext._rover.GetPosition().Should().Be(new Point(x, y));
         }
 
         [Then(@"rover should be facing ([NEWS]{1})")]
         public void ThenRoverShouldBeFacingE(string direction)
         {
-            _roverTestContext._rover.GetDirection().Should().Be(direction);
+            _roverContext._rover.GetDirection().Should().Be(direction);
         }
     }
 }
